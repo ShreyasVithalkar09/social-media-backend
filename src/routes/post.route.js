3a69e61816c6c1037c80fce7689bdc42d8e83743
ShreyasVithalkar09 <shreyas.vithalkar08@gmail.com>
2024-08-02 00:20:31 +0530

    Added get post route and modified get users posts route

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -3,6 +3,7 @@ import {verifyJwtToken} from "../middlewares/auth.middleware.js";
 import {
     createPost,
     deletePost,
+    getPostById,
     getPosts,
     getUserPosts,
     likePost,
@@ -15,8 +16,8 @@ const router = new Router();
 router.use(verifyJwtToken) // applies for all requests
 
 router.route("/").post(createPost).get(getPosts)
-router.route("/:username").get(getUserPosts)
-router.route("/:postId").delete(deletePost).put(updatePost)
+router.route("/user-posts/:username").get(getUserPosts)
+router.route("/:postId").delete(deletePost).put(updatePost).get(getPostById)
 router.route("/:postId/like").patch(likePost)
 router.route("/:postId/comments").post(addComment).get(getPostComments)
 router.route("/:postId/comments/:commentId").delete(deleteComment)
